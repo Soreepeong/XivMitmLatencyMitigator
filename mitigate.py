940a100435325968ac7a79e660481330dd0be2e9
YOxidized <79140667+YOxidized@users.noreply.github.com>
2021-06-02 17:04:43 -0400

    Update mitigate.py

--- mitigate.py
+++ mitigate.py
@@ -435,12 +435,12 @@ class Connection:
 
         # See: https://github.com/ravahn/machina/tree/NetworkStructs/Machina.FFXIV/Headers/Opcodes
         if any(ipaddress.ip_address(self.destination[0]) in x for x in INTL_DATACENTER_IP_NETWORK):
-            self.SUBTYPE_RESPONSE_ACTOR_CAST = 0x0228
-            self.SUBTYPE_RESPONSE_ACTOR_CONTROL = 0x030f
-            self.SUBTYPE_RESPONSE_ACTOR_CONTROL_SELF = 0x0200
-            self.SUBTYPE_RESPONSE_ACTION_RESULT = [0x0128, 0x0295, 0x025e, 0x0299, 0x00a7]
+            self.SUBTYPE_RESPONSE_ACTOR_CAST = 0x00a9
+            self.SUBTYPE_RESPONSE_ACTOR_CONTROL = 0x01c8
+            self.SUBTYPE_RESPONSE_ACTOR_CONTROL_SELF = 0x035a
+            self.SUBTYPE_RESPONSE_ACTION_RESULT = [0x0283, 0x025b, 0x015d, 0x0091, 0x0169]
 
-            self.SUBTYPE_REQUEST_ACTION = [0x0396, 0x0205]
+            self.SUBTYPE_REQUEST_ACTION = [0x02a2, 0x02f6]
 
             self.log(f"New[INTL]:", self.socket.getsockname(), self.socket.getpeername(), self.destination)
 
